710377cc5a7eba7dcf3631f15de713a72b7808da
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-10-17 20:05:53 +0300

    Completed Assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  let moviesList = movies.map((movie) =>{
+    return(
+      <div key={movie.title}>
+        <h2>{movie.title}</h2>
+        <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map((genre) => (<li key={genre}>{genre}</li>))}
+        </ul>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>
+        Movies Page
+      </h1>
+      {moviesList}
+    </div>
+  );
 }
 
 export default Movies;
